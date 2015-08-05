5adaeaba927d04528861461e57a5350b222cdcc6
Chris Orcutt <orcutt.chris@gmail.com>
2015-08-04 20:15:29 -0700

    Status update: tiles displaying correct date and times
    * tiles contain correct date and time
    * note: because UCLA webservices API is responding with library hours for the week starting with Monday and ending with Sunday, I had to do something slightly unintuitive -- get the timeInterval back to Monday and then populate tiles

--- Code/UCLALibrary/LibraryDisplayViewController.swift
+++ Code/UCLALibrary/LibraryDisplayViewController.swift
@@ -6,6 +6,8 @@
 //  Copyright (c) 2015 Chris Orcutt. All rights reserved.
 //
 
+let secondsInDay = 60 * 60 * 24
+
 import UIKit
 
 class LibraryDisplayViewController: UIViewController {
@@ -34,12 +36,65 @@ class LibraryDisplayViewController: UIViewController {
         verticalScrollView.contentSize =  CGSize(width: width, height: height)
     }
     
+    func timeIntervalToMonday() -> NSTimeInterval {
+        let today = NSDate()
+        var dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "EEEE";
+        var dayName = dateFormatter.stringFromDate(today)
+        
+        let daysFromMonday: Int
+        switch (dayName) {
+        case "Monday":
+            daysFromMonday = 0
+        case "Tuesday":
+            daysFromMonday = 1
+        case "Wednesday":
+            daysFromMonday = 2
+        case "Thursday":
+            daysFromMonday = 3
+        case "Friday":
+            daysFromMonday = 4
+        case "Saturday":
+            daysFromMonday = 5
+        case "Sunday":
+            daysFromMonday = 6
+        default:
+            daysFromMonday = 0
+        }
+        return NSTimeInterval(secondsInDay * -daysFromMonday)
+    }
+    
     func setupHorizontalScrollView() {
         let count = library.operatingHours?.count ?? 0
         
+        let calendar = NSCalendar.currentCalendar()
         for index in 0..<count {
+            
+            // CONote: using timeIntervalToMonday() to get the date for Monday because the UCLA webserivices
+            // API (as of August 4 2015) only responds with the library times in chunks starting on Monday
+            // and ending on Sunday
+            let date = NSDate(timeIntervalSinceNow: NSTimeInterval(Int(timeIntervalToMonday()) + index * secondsInDay))
+            let components = calendar.components(.CalendarUnitDay, fromDate: date)
+            let dayNumber = components.day
+
+            var dateFormatter = NSDateFormatter()
+            dateFormatter.dateFormat = "EEEE";
+            var dayName = dateFormatter.stringFromDate(date)
+            
+            var openingTime = "LIBRARY"
+            var closingTime = "CLOSED"
+            if let opens = library.operatingHours?[index].opens, closes = library.operatingHours?[index].closes {
+                openingTime = NSDateFormatter.localizedStringFromDate(opens, dateStyle: NSDateFormatterStyle.NoStyle, timeStyle: NSDateFormatterStyle.ShortStyle)
+                closingTime = NSDateFormatter.localizedStringFromDate(closes, dateStyle: NSDateFormatterStyle.NoStyle, timeStyle: NSDateFormatterStyle.ShortStyle)
+            }
+            
             var view = LibraryHoursView()
             view.frame.origin = CGPoint(x: CGFloat(index) * view.frame.width, y: 0)
+            view.dayOfTheWeekLabel.text = dayName
+            view.dateLabel.text = String(dayNumber)
+            view.openingTimeLabel.text = openingTime
+            view.closingTimeLabel.text = closingTime
+            
             horizontalScrollView.addSubview(view)
         }
         
